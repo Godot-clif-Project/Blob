351ccda79818498a75bf12b97c303da4afaab1fa
Manglemix <shabouza030@gmail.com>
2021-01-28 21:23:36 +0800

    possible solution for spitballbody

--- CombatScripts/spit_ball_body.gd
+++ CombatScripts/spit_ball_body.gd
@@ -5,7 +5,7 @@ extends Node2D
 
 
 export var shape_path: NodePath = "CollisionShape2D"
-export var speed := 3000.0
+export var speed := 10000.0
 export(int, LAYERS_2D_PHYSICS) var collision_mask := 3
 export var damage := 1
 export var damage_only_target := true
@@ -15,7 +15,8 @@ var target: Node2D
 var fade_time := 1.0
 var physics_query := Physics2DShapeQueryParameters.new()
 var gravity_acceleration: float = ProjectSettings.get_setting("physics/2d/default_gravity")
-var gravity_vector: Vector2 = ProjectSettings.get_setting("physics/2d/default_gravity_vector") * gravity_acceleration
+var down_vector: Vector2 = ProjectSettings.get_setting("physics/2d/default_gravity_vector")
+var gravity_vector: Vector2 = down_vector * gravity_acceleration
 var linear_velocity: Vector2
 
 onready var shape_node: CollisionShape2D = get_node(shape_path)
@@ -24,35 +25,45 @@ onready var direct_space_state := get_world_2d().direct_space_state
 
 func _ready():
 	var travel_vector: Vector2
+	var gravity_transform := Transform2D(Vector2.DOWN.angle_to(down_vector), global_transform.origin)
 	
 	if target_prediction:
 		hide()
 		set_physics_process(false)
 		
-		var last_origin := target.global_transform.origin
+		var last_origin: Vector2 = gravity_transform.xform_inv(target.global_transform.origin)
 		var last_time := OS.get_system_time_msecs()
 		yield(get_tree(), "idle_frame")
-		var new_origin := target.global_transform.origin
-		var target_velocity := (new_origin - last_origin) / (OS.get_system_time_msecs() - last_time) * 1000.0
+		travel_vector = gravity_transform.xform_inv(target.global_transform.origin)
+		var target_velocity := (travel_vector - last_origin) / (OS.get_system_time_msecs() - last_time) * 1000.0
 		
 		if target_velocity.length() > 0:
-			var o := new_origin - global_transform.origin
-			var a := target_velocity.length_squared() - speed * speed
-			var b := 2 * target_velocity.dot(o)
-			var c := o.length_squared()
-			var lead_time := (- b - sqrt(b * b - 4 * a * c)) / 2 / a
-			travel_vector = new_origin - global_transform.origin + lead_time * target_velocity
-		
-		else:
-			travel_vector = new_origin - global_transform.origin
+			travel_vector += GlobalData.quad_formula(target_velocity.length_squared() - speed * speed, 2 * target_velocity.dot(travel_vector), travel_vector.length_squared(), true) * target_velocity
 		
 		show()
 		set_physics_process(true)
 	
 	else:
-		travel_vector = target.global_transform.origin - global_transform.origin
+		travel_vector = gravity_transform.xform_inv(target.global_transform.origin)
 	
-	linear_velocity = travel_vector.normalized() * speed + Vector2.UP * gravity_acceleration / 2 * travel_vector.length() / speed
+	var t: float
+	var a := gravity_acceleration * gravity_acceleration / 4
+	var b := - gravity_acceleration * travel_vector.y
+	var c := travel_vector.length_squared() - speed * speed
+	var root1 := GlobalData.quad_formula(a, b, c, true)
+	var root2 := GlobalData.quad_formula(a, b, c, false)
+	
+	if root1 > 0 and root2 > 0:
+		t = sqrt(root1 if root1 < root2 else root2)
+	
+	elif root1 > 0 or root2 > 0:
+		t = sqrt(root1 if root1 > 0 else root2)
+	
+	if t == 0:
+		queue_free()
+		return
+	
+	linear_velocity = gravity_transform.basis_xform_inv(Vector2(travel_vector.x / t, - gravity_acceleration / 2 * t + travel_vector.y / t))
 	
 	physics_query.collision_layer = collision_mask
 	physics_query.set_shape(shape_node.shape)
