b5393b97469278801193252cfdf4fd380451c91a
Tarudahat <taruzahat@gmail.com>
2021-02-17 14:30:06 +0100

    add chandelier, fix assets "jump charm", "light"

---
+++ Actors/props/portal/portal.gd
@@ -0,0 +1,14 @@
+extends AnimatedSprite
+
+export var Destination := "TEST"
+export var Portal_id:=0
+#used to for randomization of used portals
+
+func _ready():
+	#give it a hitbox to detect if player is close enough to enter
+	pass
+	
+
+func _process(delta):
+	pass
+
